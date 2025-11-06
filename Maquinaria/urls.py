c59b4dc61ced4f819a486697098eabc14baaf834
Leonel Cortés R.- <leonel.cortes@gmail.com>
2025-11-06 12:19:26 -0300

    Agrego vista home con formulario de RUT y Horómetro

--- Maquinaria/urls.py
+++ Maquinaria/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     #path('', include('Equipos.urls')),  # Enlaza la app Equipos
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('', include('Equipos.urls')),
+    
 ]
 
