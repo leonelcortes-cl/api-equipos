267caae1192477271a3a5d072d8927ac813e3819
Leonel Cortés R.- <leonel.cortes@gmail.com>
2025-11-09 15:08:46 -0300

    Actualización de vista home y limpieza de detalle_equipo

--- Maquinaria/urls.py
+++ Maquinaria/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Equipos.urls')),  # Deriva todo hacia la app Equipos
+    path('equipos', include('Equipos.urls')),  # Deriva todo hacia la app Equipos
 ]
